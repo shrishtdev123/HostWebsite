48d3960c698bf3da96598977fecfed6e260ab7f1
Your Name <your@email.com>
2025-03-23 16:43:32 +0530

    second

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -115,7 +115,7 @@ const Collection = () => {
                <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
                <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
                     <p className='flex gap-2'>
-                     <input onChange={toggleCategy} className='w-3'  type="checkbox" value={'Men'} />Men
+                     <input onChange={toggleCategy} className='w-3'  type="checkbox" value={'MEN'} />Men
                     </p>
 
                     <p className='flex gap-2'>
