7d70b820a18c15e5dcd5b3913564b8e25f5a314f
shrishtdev <shrishtdev@gmail.com>
2025-07-06 22:31:48 +0530

    imagechanges

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -16,23 +16,17 @@ const About = () => {
            />
        </div>
 
-       <div className='my-10 flex flex-col md:flex-row gap-16'>
-         <img className='w-full md:max-w-[450px] ' src={assets.about_img} alt="" />
+       <div className="my-10 flex flex-col md:flex-row items-center gap-16 px-4">
+  <img
+    src={assets.about_img}
+    alt="About AMBA HOMES"
+    className="w-full max-w-[450px] object-cover rounded-lg shadow-md"
+  />
 
-         {/* <div className='flex flex-col  justify-center gap-6 md:w-2/4 text-gray-600'>
-             <p>
-             STARINNIGS OVERSES PRIVATE LIMITED is a new and exciting presence in the clothing industry, which is often characterized by fast-changing trends. Though we are a new entrant, our brand identity is built on a foundation of passion, creativity, and a commitment to high-quality fashion that stands the test of time.
-             </p>
-             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia consequatur itaque autem. Ratione dolorum, sed corporis perferendis maxime beatae et, eligendi iusto, asperiores earum vitae accusamus sit inventore similique doloribus.</p>
+  {/* About text section */}
+  <AboutCompany />
+</div>
 
-             <b className='text-gray-800'>Our Mission</b>
-             <p>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo hic ipsa optio odio possimus ipsam dicta incidunt saepe facere reprehenderit dolor corrupti tempora, expedita quam cupiditate pariatur assumenda labore aut!
-             </p>
-         </div> */}
-
-           <AboutCompany/>
-       </div>
 
        <div className='text-xl  py-4'>
             <Title
